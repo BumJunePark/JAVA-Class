c1c3bf0ef6f449fd27b5720bc05323ec982e3406
BumJunePark <90195079+BumJunePark@users.noreply.github.com>
2021-11-09 23:43:19 +0900

    Add files via upload

---
+++ com/my/app/Main.java
@@ -0,0 +1,10 @@
+package com.my.app;
+
+import com.my.hello.Hello;
+
+public class Main {
+    public static void main(String[] args) {
+        Hello hm = new Hello("haaaaa!");
+        hm.sayHello();
+    }
+}
